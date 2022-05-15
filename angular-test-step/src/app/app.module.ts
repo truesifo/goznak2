39fabede07649860d9bb2ed3ad6dc551a74e0386
Alex Urev <truesifo@gmail.com>
2022-05-16 00:30:50 +0300

    2

---
+++ angular-test-step/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginNgxsComponent} from './components/login-ngxs/login-ngxs.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from './material.module';
+import {IntroductionComponent} from './components/static/introduction/introduction.component';
+import {NavBarComponent} from './components/static/nav-bar/nav-bar.component';
+import {NgxsModule} from '@ngxs/store';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import {AuthState} from './services/auth-state';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginNgxsComponent,
+    IntroductionComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    NoopAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxsModule.forRoot([AuthState]),
+    NgxsStoragePluginModule.forRoot({
+      key: ['auth.username', 'auth.loggedIn']
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
